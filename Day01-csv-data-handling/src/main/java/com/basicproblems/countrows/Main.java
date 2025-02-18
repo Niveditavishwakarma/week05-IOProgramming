895bf042be2518228b1ec4ef9dcdbc8adbd5d0d2
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-02-18 16:45:43 +0530

    [Nivedita] Add . added the implementation to count the number of records present in a csv file

---
+++ Day01-csv-data-handling/src/main/java/com/basicproblems/countrows/Main.java
@@ -0,0 +1,33 @@
+package com.basicproblems.countrows;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String filepath = "C:\\Users\\asus\\OneDrive\\Desktop\\Daily Learning notes\\text.txt";
+        try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
+            int rowcount = 0;
+            String line;
+            boolean isHeader = true;
+
+            while((line = br.readLine()) != null)
+            {
+                if(isHeader)
+                {
+                    isHeader = false;
+                    continue;
+                }
+                rowcount++;
+            }
+            System.out.println("Number of records(excluding header): " + rowcount);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error reading csv file: " + e.getMessage());
+        }
+
+    }
+}
